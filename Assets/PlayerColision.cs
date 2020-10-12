cb520151ebfd2d4eb436eb0618083d3aaecb64f0
david <davidpetkov2006@gmail.com>
2020-10-12 23:54:33 +0300

    Created collision script, raycast script and added new assets.

---
+++ Assets/PlayerColision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerColision : MonoBehaviour
+{
+    public FirstPersonAIO movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "colider")
+        {
+            SceneManager.LoadScene("Mineshaft");
+        }
+    }
+
+}
---
+++ Assets/raycast.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    public float damage = 100f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
