aec8134d2bf242246d0518d80acb8244c74177db
Legodatski <ovinedev@abv.bg>
2020-10-13 12:27:19 +0300

    Updated IvoScene.

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "spaceship")
+        {
+            SceneManager.LoadScene("SpaceShip");
+        }
+
+    }
+
+
+}
