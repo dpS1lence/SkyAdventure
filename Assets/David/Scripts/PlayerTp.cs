d807ee46370ad38dc943110b1d26ae9ccd98d246
david <davidpetkov2006@gmail.com>
2020-10-22 10:15:06 +0300

    Made teleporter and changes to DavidScene.

---
+++ Assets/David/Scripts/PlayerTp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTp : MonoBehaviour
+{
+    public FirstPersonAIO movement;
+    public GameObject ui;
+    public GameObject objToTp;
+    public Transform tpLoc;
+
+    void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "other")
+        {
+            ui.SetActive(true);
+
+            objToTp.transform.position = tpLoc.transform.position;
+
+        }
+    }
+
+    void OnCollisionExit()
+    {
+        ui.SetActive(false);
+    }
+}
