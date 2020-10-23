1447805de2d621b173562a7f6b4f02f023a8bec0
david <davidpetkov2006@gmail.com>
2020-10-23 10:17:52 +0300

    Made changes to DavidScene.

---
+++ Assets/David/Scripts/UIwRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIwRaycast : MonoBehaviour
+{
+    public Camera fpsCam;
+    public GameObject uiEl;
+    public float range = 100f;
+    public float show = 100f;
+
+    RaycastHit hit;
+
+    void Start()
+    {
+        uiEl.SetActive(false);
+    }
+    void Update()
+    {
+        if (hit.collider.name == "other")
+        {
+            Debug.Log("hitted");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            uiEl.SetActive(true);
+        }
+    }
+}
