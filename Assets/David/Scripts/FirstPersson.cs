746c74d65cd0fd943e98ec8867b2eb8b53fd50d1
david <davidpetkov2006@gmail.com>
2020-11-05 20:24:41 +0200

    .

--- Assets/David/Scripts/FirstPersson.cs
+++ Assets/David/Scripts/FirstPersson.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-
-public class FPS : MonoBehaviour
+public class FirstPersson : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-    public Camera playerCamera;
+    public Camera cam;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
@@ -67,8 +65,9 @@ public class FPS : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
+
 }
