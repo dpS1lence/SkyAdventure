69306f4b7309505de2d2fc27b82a4e96ec02cf62
david <davidpetkov2006@gmail.com>
2020-11-05 23:01:44 +0200

    Legendary updated camera.

--- Assets/Ivo/Scripts/Cam.cs
+++ Assets/Ivo/Scripts/Cam.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Cam : MonoBehaviour
 {
     public Transform cam1;
     public Transform cam2;
