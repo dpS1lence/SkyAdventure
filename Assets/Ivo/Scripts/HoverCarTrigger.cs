1413d45411953e6b37f122bf8c4da82e9fce6b8a
Legodatski <ovinedev@abv.bg>
2020-11-10 08:09:30 +0200

    Added hover car.

---
+++ Assets/Ivo/Scripts/HoverCarTrigger.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverCarTrigger : MonoBehaviour
+{
+    public Transform playerCamera;
+    public GameObject player;
+    public GameObject hoverCar;
+    public GameObject hoverCarCamera;
+
+    public float range = 2;
+
+    public float spawnOffsetZ;
+    public float spawnOffsetX;
+    public float spawnOffsetY;
+
+    bool inCar = false;
+
+
+    private void Start()
+    {
+        hoverCarCamera.SetActive(false);
+        hoverCar.GetComponent<HoverCarControl>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inCar == false)
+            {
+                InstateRaycast();
+            }
+            else
+            {
+                LeaveCar();
+            }
+
+            inCar = !inCar;
+        }
+    }
+
+    public void InstateRaycast()
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, range);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "hoverCar")
+            {
+                hoverCar.GetComponent<HoverCarControl>().enabled = true;
+                hoverCarCamera.SetActive(true);
+                player.SetActive(false);
+                hoverCar.SetActive(true);
+            }
+
+        }
+    }
+
+    public void LeaveCar()
+    {
+        Vector3 spawnPosition = new Vector3(hoverCar.transform.position.x + spawnOffsetX, hoverCar.transform.position.y + spawnOffsetY, hoverCar.transform.position.z + spawnOffsetZ);
+
+        player.transform.position = spawnPosition;
+
+        player.SetActive(true);
+        hoverCarCamera.SetActive(false);
+        hoverCar.GetComponent<HoverCarControl>().enabled = false;
+    }
+}
