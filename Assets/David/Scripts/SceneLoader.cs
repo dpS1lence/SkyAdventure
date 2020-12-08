422426d19ed582a4fec28bdee6bd0b968ff2f981
david <davidpetkov2006@gmail.com>
2020-12-08 11:18:43 +0200

    Added ak-47.

--- Assets/David/Scripts/SceneLoader.cs
+++ Assets/David/Scripts/SceneLoader.cs
@@ -8,6 +8,13 @@ public class SceneLoader : MonoBehaviour
 {
 
     public string previousScene;
+    public GameObject fpsCam;
+    public float damage = 10.0f;
+    public float range = 2.0f;
+
+    void Update () {
+        Shoot();
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -19,7 +26,26 @@ public class SceneLoader : MonoBehaviour
         {
             SceneManager.LoadScene(previousScene);
         }
+        else if (collision.collider.tag == "console")
+        {
+            SceneManager.LoadScene("DavidScene_2");
+        }
+        
+    }
 
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log("raycastaqedf");
+            if(hit.collider.name == "deskColider")
+            {
+                Debug.Log("raycasta");
+                SceneManager.LoadScene("DavidScene_2");
+            }
+        }
     }
 
 
---
+++ Assets/David/Scripts/SceneLoaderSpaceShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderSpaceShip : MonoBehaviour
+{
+    public string previousScene;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("em_niceff");
+        if (collision.collider.tag == "console")
+        {
+            Debug.Log("em_nice");
+            SceneManager.LoadScene("DavidScene");
+        }
+
+    }
+}
