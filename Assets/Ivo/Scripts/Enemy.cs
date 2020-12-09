446ab771525a02be989b45e33cacc9ec2de06e52
Legodatski <ovinedev@abv.bg>
2020-12-09 17:21:47 +0200

    Updated Maze.

--- Assets/Ivo/Scripts/Enemy.cs
+++ Assets/Ivo/Scripts/Enemy.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
 
     public Transform player;
 
+    public float foundPlayer = 1;
+
+
     // Update is called once per frame
     void Update()
     {
         GetComponent<NavMeshAgent>().destination = player.transform.position;
+
+        /*if (transform.position.x < foundPlayer || transform.position.z < foundPlayer)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }*/
     }
 }
