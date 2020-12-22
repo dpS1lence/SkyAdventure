d7347f45d7ed02fcb0672b2011b5097a7c84ea9b
david <davidpetkov2006@gmail.com>
2020-12-23 00:42:07 +0200

    Changes to DavidScene2, made shooter scene :)

---
+++ Assets/David/Scripts/ScriptForGun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptForGun : MonoBehaviour
+{
+    public GameObject cameraff;
+    public float range3 = 2.0f;
+    public GameObject enemy;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Shoot();
+    }
+
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(cameraff.transform.position, cameraff.transform.forward, out hit, range3);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.name == "ColiderChest")
+            {
+                if(Input.GetButtonDown("Fire1"))
+                {
+                    Destroy(enemy);
+                }
+            }
+        }
+    }
+}
