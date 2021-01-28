435ee4f4911312e6a7a751ce11b9ebc66800f7bf
Legodatski <ovinedev@abv.bg>
2021-01-28 18:58:03 +0200

    I tried but nothings happened.

---
+++ Assets/Ivo/Scripts/Throw.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    public GameObject objectToThrow;
+
+    public float thrust = 1.0f;
+
+    public Rigidbody rb;
+
+    public Transform camera;
+
+    void Start()
+    {
+        rb = objectToThrow.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(camera.forward * thrust);
+    }
+}
