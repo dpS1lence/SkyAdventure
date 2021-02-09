d81e469b4db7a5c5d22279df8d7b63e0f99f2a0d
David Petkov <davidpetkov2006@gmail.com>
2021-02-09 09:46:50 +0200

    Updated DavidScene_2

--- Assets/David/Scripts/PlayerColision.cs
+++ Assets/David/Scripts/PlayerColision.cs
@@ -3,13 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerColision : MonoBehaviour
 {
-    /*public FirstPersonAIO movement;
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.name == "colider")
         {
             SceneManager.LoadScene("Mineshaft");
         }
-    }*/
-
+    }
 }
