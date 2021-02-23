690e0b7cdd1fe62db01f2bc1af4f5947431dbb6d
David Petkov <davidpetkov2006@gmail.com>
2021-02-23 15:59:12 +0200

    Fixed details.

---
+++ Assets/David/Scripts/enemyDieTrash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDieTrash : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject fpsCam;
+    public GameObject tntmain;
+    public float range = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+
+            }
+        }
+    }
+}
