73a560c53a633b6acc7a874d3b7282abd69140c9
Legodatski <ovinedev@abv.bg>
2021-02-27 15:20:36 +0200

    Better music

--- Assets/Ivo/Scripts/SoundPlayer.cs
+++ Assets/Ivo/Scripts/SoundPlayer.cs
@@ -5,49 +5,17 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     public GameObject steps;
-    public GameObject jump;
-
-    bool grounded;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            if (grounded)
-            {
-                steps.SetActive(true);
-
-            }
-            else
-            {
-                steps.SetActive(false);
-            }
+            steps.SetActive(true);
         }
         else
         {
             steps.SetActive(false);
-        }
-
-        /*if (Input.GetKey(KeyCode.Space))
-        {
-            jump.SetActive(true);
-        }
-        else
-        {
-            jump.SetActive(false);
-        }*/
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.collider.gameObject.layer == 9)
-        {
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
         }
     }
 }
