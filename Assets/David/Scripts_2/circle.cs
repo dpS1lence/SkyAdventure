831b5f4a230f98a5eb573f86882c289d29a82043
David Petkov <davidpetkov2006@gmail.com>
2021-05-05 22:17:05 +0300

    circle

--- Assets/David/Scripts_2/circle.cs
+++ Assets/David/Scripts_2/circle.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class circle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject circle1;
+    public GameObject circle2;
+    public GameObject fpsCam;
+    public float range = 15f;
+    
+void Update()
+{
+    RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.collider.name == "consol")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    circle1.SetActive(false);
+                    circle2.SetActive(true);
+                }
+            }
+        }
+}
+    private void OnTriggerEnter(Collider other) 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.name == "Circle1")
+        {
+            SceneManager.LoadScene("Maze");
+        }
     }
 }
